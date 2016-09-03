4692de58c2399c53461bd8bb2015c54519e87ab1
Andy Stiller <andy@andyhat.co.uk>
2016-09-03 16:23:12 +0100

    Remove old set-up folders.

--- 16.04lts/ansible-roles.sh
+++
@@ -1,7 +0,0 @@
-ansible-galaxy install geerlingguy.apache
-ansible-galaxy install geerlingguy.mysql
-ansible-galaxy install geerlingguy.php
-ansible-galaxy install geerlingguy.php-mysql
-ansible-galaxy install geerlingguy.php-xdebug
-ansible-galaxy install geerlingguy.nodejs
-ansible-galaxy install geerlingguy.ruby
